bd67b343d5500b8904fd8990ce09f8ae930804b8
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-07-29 16:53:25 -0300

    fix: getCustomerById - TO_CHAR

--- src/controllers/customersControllers.js
+++ src/controllers/customersControllers.js
@@ -36,8 +36,10 @@ export async function  getCustomerById(req, res){
     const {id}= req.params;
     try{      
 
-        const customer = await db.query(`SELECT * FROM customers 
-            WHERE id = $1`, [id]);
+        const customer = await db.query(`
+            SELECT id, name, phone, cpf, TO_CHAR(birthday, 'YYYY-MM-DD') AS birthday
+                FROM customers 
+                WHERE id = $1`, [id]);
         if(!customer.rows[0])return res.sendStatus(404);
         res.send(customer.rows[0]);
     }catch(error){
